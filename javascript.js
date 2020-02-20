ddbec4e8853d4e8917b0a8dd45e38ff4a88f1d5d
developerpritam <pritambag426@gmail.com>
2020-02-20 10:18:56 +0530

    github portfolio
     i am happy

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+// $(document).ready(function(){
+//     $(".navbar-toggler").click(function(){
+//         $(".navbar-nav").css("transition","0.3s");
+        
+//     });
+// });
+
+// loading animation ////////////////////////////////////////
+
+
+
+
+// loading animation ////////////////////////////////////////
