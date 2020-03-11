9d2f9adf7336867775c5c223c1a62eb3d4edb03c
developerpritam <51673156+developerpritam@users.noreply.github.com>
2020-03-11 16:16:31 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,13 @@
-
-// loading animation ////////////////////////////////////////
-
-
-
-
-// loading animation ////////////////////////////////////////
-
-
 // sticky navbar animation
 
 window.addEventListener("scroll", function(){
     let menuarea = document.getElementById('menuarea');
 
-    if(window.pageYOffset > 0){
+    if(window.pageYOffset > 1){
         menuarea.classList.add("cus-nav");
-
-    }
+        }
     else{
         menuarea.classList.remove("cus-nav")
     }
 });
+// sticky navbar animation
